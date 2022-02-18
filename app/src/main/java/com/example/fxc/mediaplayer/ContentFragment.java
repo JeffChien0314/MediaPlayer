7d806668e45b64cceb21dd6d684f521d7fd5eef0
JenniferSunLiJuan <37397181@qq.com>
2022-02-18 11:25:54 +0800

    加入播放动画的资源及修改了上一曲，下一曲的点击无功能的问题

--- app/src/main/java/com/example/fxc/mediaplayer/ContentFragment.java
+++ app/src/main/java/com/example/fxc/mediaplayer/ContentFragment.java
@@ -1,6 +1,8 @@
 package com.example.fxc.mediaplayer;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,15 +11,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import com.example.fxc.mediaplayer.R;
+
 import com.shuyu.gsyvideoplayer.model.GSYVideoModel;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
 /**
  * Created by Jennifer on 2022/2/08.
  */
@@ -32,33 +35,38 @@ public class ContentFragment extends Fragment {
     private ListView mediaFile_list;
     public List<MediaInfo> mediaInfos = null;
     private List<GSYVideoModel> urls = new ArrayList<>();
+    //private ImageView playing_icon;
+    private TextView totaltime;
+    private AnimationDrawable ani_gif_playing;
+    public MediaListAdapter listAdapter;
 
-    public MediaListAdapter Listadapter;
     public ContentFragment() {
         super();
     }
+
     @SuppressLint("ValidFragment")
     public ContentFragment(Context context, List<HashMap<String, String>> list) {
         super();
         mContext = context;
         mList = list;
     }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.contentfragment, container, false);
         //音視頻列表
-        mediaFile_list = (ListView)view.findViewById(R.id.list);
-        String pathDefault= ((MainActivity)getActivity()).getCurrentStoragePath();//默認顯示當前設備的多媒體文件
-        mediaInfos = MediaUtil.getMediaInfos(0,mContext,pathDefault);
-        Listadapter = new MediaListAdapter(mContext, mediaInfos);
-        mediaFile_list.setAdapter(Listadapter);
+        mediaFile_list = (ListView) view.findViewById(R.id.list);
+        String pathDefault = ((MainActivity) getActivity()).getCurrentStoragePath();//默認顯示當前設備的多媒體文件
+        mediaInfos = MediaUtil.getMediaInfos(0, mContext, pathDefault);
+        listAdapter = new MediaListAdapter(mContext, mediaInfos);
+        mediaFile_list.setAdapter(listAdapter);
         mediaFile_list.setOnItemClickListener(onItemClickListener);
         return view;
     }
 
     public List<GSYVideoModel> getUrls() {
-        for (int i=0;i<mediaInfos.size();i++){
+        for (int i = 0; i < mediaInfos.size(); i++) {
             urls.add(mediaInfos.get(i).getGsyVideoModel());
         }
         return urls;
@@ -72,30 +80,45 @@ public class ContentFragment extends Fragment {
 
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Log.i("main", "Jennifertest35=: " + view);
+            view.findViewById(R.id.playing_icon).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.playing_icon).setBackgroundResource(R.drawable.ani_gif_playing);
+            ani_gif_playing = (AnimationDrawable)(view.findViewById(R.id.playing_icon)).getBackground();
+            ani_gif_playing.start();
+            view.findViewById(R.id.totalTime).setVisibility(View.INVISIBLE);
             ((MainActivity)getActivity()).playMusic(position);
         }
     };
+
     public void smoothScrollToPosition(int position) {
         Log.i("main", "Jennifertest21=: " + position);
-        mediaFile_list.smoothScrollToPosition( position);
+        mediaFile_list.smoothScrollToPosition(position);
         mediaFile_list.performItemClick(
                 mediaFile_list.getAdapter().getView(position, null, null),
                 position,
                 mediaFile_list.getItemIdAtPosition(position));
     }
-    public void updateMediaList(int mediaType,String path){
-        if (mediaType==0){//音樂
-            mediaInfos  = MediaUtil.getMusicInfos(mContext,path);
-        }else {//視頻
-            mediaInfos = MediaUtil.getVideoInfos(mContext,path);
+
+    public void playingAnimation(int position) {
+        ImageView playing_icon = (ImageView) listAdapter.getView(position, null, null).findViewById(R.id.playing_icon);
+        playing_icon.setVisibility(View.INVISIBLE);
+        listAdapter.getView(position, null, null).invalidate();
+        //totaltime=(TextView)mediaFile_list.getAdapter().getView(position,null,null).findViewById(R.id.totalTime);
+    }
+
+    public void updateMediaList(int mediaType, String path) {
+        if (mediaType == 0) {//音樂
+            mediaInfos = MediaUtil.getMusicInfos(mContext, path);
+        } else {//視頻
+            mediaInfos = MediaUtil.getVideoInfos(mContext, path);
         }
-        Listadapter = new MediaListAdapter(mContext, mediaInfos);
-        if (mediaFile_list==null)return;
-        mediaFile_list.setAdapter(Listadapter);
-        Listadapter.notifyDataSetChanged();
-        if (urls!=null && urls.size()!=0){
+        listAdapter = new MediaListAdapter(mContext, mediaInfos);
+        if (mediaFile_list == null) return;
+        mediaFile_list.setAdapter(listAdapter);
+        listAdapter.notifyDataSetChanged();
+        if (urls != null && urls.size() != 0) {
             urls.clear();
-            for (int i=0;i<mediaInfos.size();i++){
+            for (int i = 0; i < mediaInfos.size(); i++) {
                 urls.add(mediaInfos.get(i).getGsyVideoModel());
             }
         }
